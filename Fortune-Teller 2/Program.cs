91372ae03fb0f248d1c61fe523bad136d4c9d32d
JCarg <Jcarg108@gmail.com>
2017-01-13 15:09:03 -0500

    Retirement finished partial vacation done

--- Fortune-Teller 2/Program.cs
+++ Fortune-Teller 2/Program.cs
@@ -13,17 +13,40 @@ namespace Fortune_Teller_2
             Console.WriteLine("Welcome to the most accurate virtual fortune telling system on the web.");
             Console.WriteLine("Let's Begin!\a");
 
-            //Name
+            //              Name Info
             Console.WriteLine("What is your first name? Type answers below.");
             string firstname = Console.ReadLine();
             Console.WriteLine("\nWhat is your last name?");
             string lastname = Console.ReadLine();
-            name(firstname, lastname);
+            greetingsname(firstname, lastname);
+
+            //              Age Info
+            Console.WriteLine("\nWhat is your age?");
+            int age = int.Parse(Console.ReadLine());
+
 
         }
-        static void name(string firstname, string lastname)
+        //              Name Method
+        static void greetingsname(string firstname, string lastname)
         {
-            Console.WriteLine("Greetings " + firstname + " " + lastname + ". I will tell you your future./a");
+            Console.WriteLine("Greetings " + firstname + " " + lastname + "! I will tell you your future./a");
         }
+
+        //              Age Method
+        static int retirementage(int age)
+        {
+            string years = " ";
+            string ageodd = "800 Years";
+            string ageeven = "5 years";
+            if (age % 2 > 0)
+            {
+                years = ageodd;
+            }
+            else
+            {
+                years = ageeven;
+            }
+        }
+
     }
 }
