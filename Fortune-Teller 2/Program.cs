ad5d7219c336ac71082a7d9cbd2c301b3ba0f071
JCarg <Jcarg108@gmail.com>
2017-01-13 14:41:41 -0500

    Name section done

--- Fortune-Teller 2/Program.cs
+++ Fortune-Teller 2/Program.cs
@@ -10,6 +10,20 @@ namespace Fortune_Teller_2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the most accurate virtual fortune telling system on the web.");
+            Console.WriteLine("Let's Begin!\a");
+
+            //Name
+            Console.WriteLine("What is your first name? Type answers below.");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("\nWhat is your last name?");
+            string lastname = Console.ReadLine();
+            name(firstname, lastname);
+
+        }
+        static void name(string firstname, string lastname)
+        {
+            Console.WriteLine("Greetings " + firstname + " " + lastname + ". I will tell you your future./a");
         }
     }
 }
